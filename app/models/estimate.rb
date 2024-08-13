ef39e1b0c1251f6bb6be192542a0a38e1003ebe4
Makoto Kitamura <admin@mktktmr.net>
2024-08-13 17:46:17 +0900

    setup closure_tree

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,2 +1,3 @@
 class Estimate < ApplicationRecord
+  has_closure_tree_root :root_estimate_detail
 end
--- app/models/estimate_detail.rb
+++ app/models/estimate_detail.rb
@@ -1,3 +1,5 @@
 class EstimateDetail < ApplicationRecord
+  has_closure_tree order: 'order_number', numeric_order: true, dont_order_roots: true
+
   belongs_to :estimate
 end
